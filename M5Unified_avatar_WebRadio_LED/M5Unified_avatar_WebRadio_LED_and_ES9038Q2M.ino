392ea702d4bac036d5c22b94b4616a62e354d7bc
riaosan <fiatpanda750@icloud.com>
2022-06-18 19:41:36 +0900

    192k->160k

--- M5Unified_avatar_WebRadio_LED/M5Unified_avatar_WebRadio_LED_and_ES9038Q2M.ino
+++ M5Unified_avatar_WebRadio_LED/M5Unified_avatar_WebRadio_LED_and_ES9038Q2M.ino
@@ -620,7 +620,7 @@ void setupAudio(void) {
   {  // custom setting
     auto spk_cfg = M5.Speaker.config();
     //  sample_rateを上げると、CPU負荷が上がる代わりに音質が向上します。
-    spk_cfg.sample_rate      = 192000 * 2;  // default:64000 (64kH z)  e.g. 48000 , 50000 , 80000 , 96000 , 100000 , 128000 , 144000 , 192000 , 200000
+    spk_cfg.sample_rate      = 160000 * 2;  // default:64000 (64kH z)  e.g. 48000 , 50000 , 80000 , 96000 , 100000 , 128000 , 144000 , 192000 , 200000
     spk_cfg.task_pinned_core = APP_CPU_NUM;
     spk_cfg.i2s_port         = i2s_port_t::I2S_NUM_1;  // CVBS use IS2_NUM_0. Audio use I2S_NUM_1.
     spk_cfg.pin_bck          = 33;
